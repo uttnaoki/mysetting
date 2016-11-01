c597af4f2dc69ffe8a2a84278212999b2f18e8ef
uttnaoki <uttnaoki@gmail.com>
2016-11-01 12:40:11 +0900

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -e
 
-today=`date +"%Y/%m/%d %H:%M:%S"`
+if [ $1 = '-m' ];then
+	read COMMENT
+else
+	COMMENT=`date +"%Y/%m/%d %H:%M:%S"`
+fi
 
 git add .
-git commit -m "$today"
+git commit -m "$COMMENT"
 git push origin master
