b9331baa1aa586c4bb73413bcb4e6a0a80dcde67
Naoki Kinoshita <uttnaoki@gmail.com>
2016-12-18 14:07:11 +0900

    Update csv2textable.sh

--- scripts/csv2textable.sh
+++ scripts/csv2textable.sh
@@ -1,7 +1,6 @@
 #!bin/sh
 cat $1 | while read line; do
-    tmp=`echo -n $line | sed -e "s/,/ \& /g"`
-    echo -n $tmp
+    echo -n $line | sed -e "s/,/ \& /g"
     echo -n "\\" 
     echo -n "\\" 
     echo "\\hline" 
