236ff7817b433d131b12a67a1cc4ae548d7fc239
Naoki Kinoshita <uttnaoki@gmail.com>
2017-07-27 17:41:56 +0900

    Mod scrap.py

--- WebScraping/dynamicHTML/scrap.py
+++ WebScraping/dynamicHTML/scrap.py
@@ -3,12 +3,12 @@ from selenium import webdriver
 driver = webdriver.PhantomJS()
 driver.get('http://judge.u-aizu.ac.jp/onlinejudge/user.jsp?id=tozangezan#2')
 
-pages = driver.find_elements_by_id('sp_1_pager')[0].text
-next_button = driver.find_elements_by_id('next_pager')[0]
+pages = driver.find_element_by_id('sp_1_pager').text
+next_button = driver.find_element_by_id('next_pager')
 
 data = []
 for i in range(int(pages)):
-    rows = driver.find_elements_by_id('list5')[0].text.split('\n')
+    rows = driver.find_element_by_id('list5').text.split('\n')
     for row in rows:
         elems = row.split(' ')
         data.append(tuple(elems))
