b2257da383d9eb88bfe8558dc16a6ce2f7751226
Naoki Kinoshita <uttnaoki@gmail.com>
2022-06-17 15:36:48 +0900

    色々削除

--- scripts/csv2textable.sh
+++
@@ -1,7 +0,0 @@
-#!bin/sh
-cat $1 | while read line; do
-    echo -n $line | sed -e "s/,/ \& /g"
-    echo -n "\\" 
-    echo -n "\\" 
-    echo "\\hline" 
-done
--- scripts/makesh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-cp /media/sf_debian/report.tex .
-make
-make
-cp report.pdf /media/sf_debian/
